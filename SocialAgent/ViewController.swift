01e844690bcc139ad7cba172375dca18f22a69c1
Corey <cph52b@mail.missouri.edu>
2016-04-12 19:15:04 -0500

    remove facebook

--- SocialAgent/ViewController.swift
+++ SocialAgent/ViewController.swift
@@ -11,31 +11,16 @@ import FBSDKLoginKit
 import TwitterKit
 
 
-class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+class ViewController: UIViewController {
     
     @IBAction func unwindToViewController (sender: UIStoryboardSegue){
         self.dismissViewControllerAnimated(true, completion: nil)
         
     }
-    @IBOutlet weak var facebookImage: UIImageView!
-    @IBOutlet weak var twitterImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if (FBSDKAccessToken.currentAccessToken() != nil)
-        {
-            // User is already logged in, do work such as go to next view controller.
-        }
-        else
-        {
-            let loginView : FBSDKLoginButton = FBSDKLoginButton()
-            self.view.addSubview(loginView)
-            //loginView.frame=CGRectMake(0,0,180,40);
-            loginView.center = self.view.center
-            loginView.readPermissions = ["public_profile", "email", "user_friends"]
-            loginView.delegate = self
-        }
         
         let logInButton = TWTRLogInButton { (session, error) in
             if let unwrappedSession = session {
@@ -51,7 +36,7 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         }
         
         // TODO: Change where the log in button is positioned in your view
-        logInButton.center = self.view.center
+        logInButton.center = CGPoint(x: 190, y: 480) //self.view.center
         self.view.addSubview(logInButton)
 
     }
@@ -61,51 +46,8 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func returnUserData()
-    {
-        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
-        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
-            
-            if ((error) != nil)
-            {
-                // Process error
-                print("Error: \(error)")
-            }
-            else
-            {
-                print("fetched user: \(result)")
-                let userName : NSString = result.valueForKey("name") as! NSString
-                print("User Name is: \(userName)")
-                let userEmail : NSString = result.valueForKey("email") as! NSString
-                print("User Email is: \(userEmail)")
-            }
-        })
-    }
     
     // Facebook Delegate Methods
-    
-    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        print("User Logged In")
-        
-        if ((error) != nil)
-        {
-            // Process error
-        }
-        else if result.isCancelled {
-            // Handle cancellations
-        }
-        else {
-            // If you ask for multiple permissions at once, you
-            // should check if specific permissions missing
-            if result.grantedPermissions.contains("email")
-            {
-                // Do work
-            }
-        }
-    }
-    
-    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
-        print("User Logged Out")
-    }
+
 }
 
